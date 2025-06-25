c6a9894db3139d6d8f19ef29582ded16b5978c41
joacoox <joaquinarieldominguez@gmail.com>
2025-06-25 01:04:37 -0300

    url fix

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-  api_url: 'api-red-social-nest.vercel.app/',
+  api_url: 'https://api-red-social-nest.vercel.app/',
   urlSupaBase: "https://ckxrkxczguzjcfemnjve.supabase.co",
   tokenSupaBase: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNreHJreGN6Z3V6amNmZW1uanZlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQzMjYzNTMsImV4cCI6MjA1OTkwMjM1M30.2D_p1LgxkB3ZTdAPGXVytbldwf8Ws9QtTn1QXi2VgkI"
 };
